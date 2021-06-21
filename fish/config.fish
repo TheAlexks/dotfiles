f22231de7ff596baa07c8b4bf7e5939a66c97fb0
Alex <hackersfromtheshadow@gmail.com>
2021-06-21 22:19:54 +0200

    Edited /haskell-org/haskell.org file.

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+#Fish config by Alex
+
+# Usually, there's a welcome message, this line disables it. 
+set fish_greeting
+
+# Startship promt
+  #Installation
+    #curl -fsSL https://starship.rs/install.sh
+
+# Using it
+startship init fish | source
